e4666db857d43542e85c7b8342fabf7141fcaaa1
PartTimeUNICAH <157434031+PartTimeCoding@users.noreply.github.com>
2025-07-29 19:51:51 +0000

    Revert "Modificacion de los scripts para la base de datos"
    This reverts commit 0431ffab224d205c7548cf93856cd37e54ec8b30.

---
+++ docs/scripts/05_roles.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Roles (
+                       Cod_Rol INT AUTO_INCREMENT PRIMARY KEY,
+                       NombreRol VARCHAR(50) NOT NULL
+);
---
+++ docs/scripts/06_funciones.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Funciones (
+                           Cod_Funcion INT AUTO_INCREMENT PRIMARY KEY,
+                           NombreFuncion VARCHAR(100) NOT NULL,
+                           Descripcion VARCHAR(255)
+);
