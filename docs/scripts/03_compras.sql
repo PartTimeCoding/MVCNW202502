d33239894ec13331be5ae3f888ba5df89e31818d
PartTimeUNICAH <157434031+PartTimeCoding@users.noreply.github.com>
2025-07-29 19:55:52 +0000

    Modificacion de los scripts para la base de datos 1.1

---
+++ docs/scripts/03_compras.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `ordenes` (
+    `ordenId` INT AUTO_INCREMENT PRIMARY KEY,
+    `usercod` BIGINT(10) NOT NULL,
+    `fechaOrden` DATETIME DEFAULT NOW(),
+    `estado` ENUM('Pendiente', 'Completado', 'Cancelado', 'Enviado') DEFAULT 'Pendiente',
+    `montoTotal` DECIMAL(12, 2) NOT NULL,
+    FOREIGN KEY (`usercod`) REFERENCES `usuario`(`usercod`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `pedidos` (
+    `pedidoId` INT AUTO_INCREMENT PRIMARY KEY,
+    `ordenId` INT NOT NULL,
+    `libroId` INT NOT NULL,
+    `cantidad` INT NOT NULL,
+    `precioUnitario` DECIMAL(12, 2) NOT NULL,
+    FOREIGN KEY (`ordenId`) REFERENCES `ordenes`(`ordenId`),
+    FOREIGN KEY (`libroId`) REFERENCES `libros`(`libroId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `transacciones` (
+    `transaccionId` INT AUTO_INCREMENT PRIMARY KEY,
+    `ordenId` INT NOT NULL,
+    `fechaTransaccion` DATETIME DEFAULT NOW(),
+    `estado` ENUM('Aprobado', 'Rechazado', 'pendiente'),
+    FOREIGN KEY (`ordenId`) REFERENCES `ordenes`(`ordenId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
