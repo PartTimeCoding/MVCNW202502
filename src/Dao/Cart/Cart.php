fb7e1c02239380fb2d43a63808b54a5a26b4fba1
Ana Gladys Cordova Avila <anag_cordovaa@unicah.edu>
2025-07-29 21:09:50 -0600

    Dao Cart

--- src/Dao/Cart/Cart.php
+++ src/Dao/Cart/Cart.php
@@ -5,16 +5,12 @@ use Dao\Table;
 
 class Cart extends Table
 {
-    /**
-     * Obtiene todos los productos disponibles con stock actualizado
-     * considerando los productos reservados en las carretillas.
-     */
+
     public static function getProductosDisponibles()
     {
         $sqlAllProductos = "SELECT * FROM products WHERE productStatus = 'ACT';";
         $productos = self::obtenerRegistros($sqlAllProductos);
 
-        // Stock reservado (carretilla de usuarios logueados)
         $sqlCarretilla = "SELECT productId, SUM(crrctd) AS reserved
                           FROM carretilla
                           WHERE TIME_TO_SEC(TIMEDIFF(NOW(), crrfching)) <= :delta
@@ -23,7 +19,6 @@ class Cart extends Table
             "delta" => \Utilities\Cart\CartFns::getAuthTimeDelta()
         ]);
 
-        // Stock reservado (carretilla anónima)
         $sqlCarretillaAnon = "SELECT productId, SUM(crrctd) AS reserved
                               FROM carretillaanon
                               WHERE TIME_TO_SEC(TIMEDIFF(NOW(), crrfching)) <= :delta
@@ -32,7 +27,6 @@ class Cart extends Table
             "delta" => \Utilities\Cart\CartFns::getUnAuthTimeDelta()
         ]);
 
-        // Ajustar el stock
         $productosAjustados = [];
         foreach ($productos as $prod) {
             $productosAjustados[$prod["productId"]] = $prod;
@@ -47,12 +41,36 @@ class Cart extends Table
         return array_values($productosAjustados);
     }
 
-    /**
-     * Obtiene el detalle de un producto por ID.
-     */
+
     public static function getProductoById($productId)
     {
         $sql = "SELECT * FROM products WHERE productId = :id LIMIT 1;";
         return self::obtenerUnRegistro($sql, ["id" => $productId]);
+    }
+
+
+    public static function getAuthCart($userId)
+    {
+        $sql = "SELECT c.productId, p.productName, p.productDescription,
+                       c.crrctd, c.crrprc
+                FROM carretilla c
+                INNER JOIN products p ON p.productId = c.productId
+                WHERE c.userId = :userId;";
+        return self::obtenerRegistros($sql, ["userId" => $userId]);
+    }
+
+
+    public static function addToAuthCart($productId, $userId, $amount, $price)
+    {
+        $sql = "INSERT INTO carretilla (userId, productId, crrctd, crrprc, crrfching)
+                VALUES (:userId, :productId, :amount, :price, NOW())
+                ON DUPLICATE KEY UPDATE crrctd = crrctd + :amount, crrfching = NOW();";
+
+        return self::executeNonQuery($sql, [
+            "userId"    => $userId,
+            "productId" => $productId,
+            "amount"    => $amount,
+            "price"     => $price
+        ]);
     }
 }
